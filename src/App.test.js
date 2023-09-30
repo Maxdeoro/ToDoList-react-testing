ae7140de7fa37fd06ff607268755471974ebe189
Maxdeoro <maxdeoro@gmail.com>
2023-09-30 23:21:43 +0300

    some changes in style and App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/getting started/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('<App/> have rendered correctly', () => {
+  it ('Component <App/> renders correctly', () => {
+    const {getByText} = render(<App/>);
+    expect(screen.getByText(/Getting started with React Testing Library/i))
+          .toBeInTheDocument();
+  });
 });
